f78a51c8af11da037ac126b792b26752fcd9ee8b
davrepo8513 <tyagidav97@gmail.com>
2025-07-28 03:24:22 +0530

    feat: setup divaastra-be project

---
+++ divaastra-be/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    caption = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username}"
